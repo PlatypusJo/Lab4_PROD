c3d3f1ddb0e90cec9f0d768df450ebe40a74fba3
PlatypusJo <maxim_muravyov@mail.ru>
2025-04-04 17:14:04 +0300

    Add BalanceSolver methods

--- QuadraticOptimizationSolver/DataModels/BalanceDataModel.cs
+++ QuadraticOptimizationSolver/DataModels/BalanceDataModel.cs
@@ -8,5 +8,10 @@ namespace QuadraticOptimizationSolver.DataModels
 {
     public class BalanceDataModel : BaseDataModel
     {
+        public double[,] MatrixA { get; set; }
+        public double[] VectorY { get; set; }
+        public double[] Tolerance { get; set; }
+        public double[] VectorI { get; set; }
+        public double[] VectorX0 { get; set; }
     }
 }
