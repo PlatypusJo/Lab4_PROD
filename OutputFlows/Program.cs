492ece2d726055b8890fbbda7c1bb6c7dfb83337
PlatypusJo <maxim_muravyov@mail.ru>
2025-04-05 17:29:28 +0300

    Add new Test, refactoring

--- OutputFlows/Program.cs
+++ OutputFlows/Program.cs
@@ -12,6 +12,7 @@ namespace OutputFlows
             IQuadraticOptimizationSolver<BalanceDataModel> solver = new BalanceSolver();
             OutputOriginal(solver);
             OutputV1(solver);
+            OutputV1WithConditions(solver);
         }
 
         static private void OutputOriginal(IQuadraticOptimizationSolver<BalanceDataModel> solver)
@@ -54,6 +55,30 @@ namespace OutputFlows
 
             double[] result = solver.Solve(V1);
             Console.WriteLine("V1: ");
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine($"x{i} = {result[i]:F3}");
+            }
+        }
+
+        static private void OutputV1WithConditions(IQuadraticOptimizationSolver<BalanceDataModel> solver)
+        {
+            BalanceDataModel V1WithConditions = new BalanceDataModel()
+            {
+                MatrixA = new double[,] {
+                    { 1, -1, -1, 0, 0, 0, 0, -1 },
+                    { 0, 0, 1, -1, -1, 0, 0, 0 },
+                    { 0, 0, 0, 0, 1, -1, -1, 0 },
+                    { -1, 10, 0, 0, 0, 0, 0, 0 },
+                },
+                VectorY = new double[] { 0, 0, 0, 0 },
+                Tolerance = new double[] { 0.200, 0.121, 0.683, 0.040, 0.102, 0.081, 0.020, 0.667 },
+                VectorI = new double[] { 1, 1, 1, 1, 1, 1, 1, 1 },
+                VectorX0 = new double[] { 10.005, 3.033, 6.831, 1.985, 5.093, 4.057, 0.991, 6.667 }
+            };
+
+            double[] result = solver.Solve(V1WithConditions);
+            Console.WriteLine("V1 with conditions: ");
             for (int i = 0; i < result.Length; i++)
             {
                 Console.WriteLine($"x{i} = {result[i]:F3}");
--- QuadraticOptimizationLibTests/BalanceSolverTests.cs
+++ QuadraticOptimizationLibTests/BalanceSolverTests.cs
@@ -8,25 +8,20 @@ namespace QuadraticOptimizationLibTests
 {
     public class BalanceSolverTests
     {
+        #region Поля
+
         const double accuracy = 0.0001;
+
+        #endregion
+        
+        #region Тесты
 
         [Fact]
         public void SolveOriginal_PassCorrectParameters_GetExpectedResult()
         {
             // Arrange
             IQuadraticOptimizationSolver<BalanceDataModel> solver = new BalanceSolver();
-            BalanceDataModel dataEntity = new BalanceDataModel()
-            {
-                MatrixA = new double[,] {
-                    { 1, -1, -1, 0, 0, 0, 0 },
-                    { 0, 0, 1, -1, -1, 0, 0 },
-                    { 0, 0, 0, 0, 1, -1, -1 },
-                },
-                VectorY = new double[] { 0, 0, 0 },
-                Tolerance = new double[] { 0.200, 0.121, 0.683, 0.040, 0.102, 0.081, 0.020 },
-                VectorI = new double[] { 1, 1, 1, 1, 1, 1, 1 },
-                VectorX0 = new double[] { 10.005, 3.033, 6.831, 1.985, 5.093, 4.057, 0.991 }
-            };
+            BalanceDataModel dataEntity = GetDataModelOriginal();
 
             // Act
             double[] result = solver.Solve(dataEntity);
@@ -43,6 +38,58 @@ namespace QuadraticOptimizationLibTests
         {
             // Arrange
             IQuadraticOptimizationSolver<BalanceDataModel> solver = new BalanceSolver();
+            BalanceDataModel dataEntity = GetDataModelV1();
+
+            // Act
+            double[] result = solver.Solve(dataEntity);
+            var A = Matrix<double>.Build.SparseOfArray(dataEntity.MatrixA);
+            var x = Vector<double>.Build.SparseOfArray(result);
+            double[] actual = A.Multiply(x).ToArray();
+
+            // Assert
+            Assert.All(actual, item => Assert.True(item <= accuracy));
+        }
+
+        [Fact]
+        public void SolveV1WithConditions_PassCorrectParameters_FirstFlowLargerSecond10Times()
+        {
+            //Arrange:
+            int expectedDifference = 10;
+            IQuadraticOptimizationSolver<BalanceDataModel> solver = new BalanceSolver();
+            BalanceDataModel dataEntity = GetDataModelV1WithConditions();
+
+            // Act:
+            double[] result = solver.Solve(dataEntity);
+            int actualDifference = (int)(Math.Round(result[0] / result[1], MidpointRounding.AwayFromZero));
+
+            // Assert:
+            Assert.Equal(actualDifference, expectedDifference);
+        }
+
+        #endregion
+
+        #region Внутренние методы
+
+        private BalanceDataModel GetDataModelOriginal()
+        {
+            BalanceDataModel dataEntity = new BalanceDataModel()
+            {
+                MatrixA = new double[,] {
+                    { 1, -1, -1, 0, 0, 0, 0 },
+                    { 0, 0, 1, -1, -1, 0, 0 },
+                    { 0, 0, 0, 0, 1, -1, -1 },
+                },
+                VectorY = new double[] { 0, 0, 0 },
+                Tolerance = new double[] { 0.200, 0.121, 0.683, 0.040, 0.102, 0.081, 0.020 },
+                VectorI = new double[] { 1, 1, 1, 1, 1, 1, 1 },
+                VectorX0 = new double[] { 10.005, 3.033, 6.831, 1.985, 5.093, 4.057, 0.991 }
+            };
+
+            return dataEntity;
+        }
+
+        private BalanceDataModel GetDataModelV1()
+        {
             BalanceDataModel dataEntity = new BalanceDataModel()
             {
                 MatrixA = new double[,] {
@@ -56,14 +103,28 @@ namespace QuadraticOptimizationLibTests
                 VectorX0 = new double[] { 10.005, 3.033, 6.831, 1.985, 5.093, 4.057, 0.991, 6.667 }
             };
 
-            // Act
-            double[] result = solver.Solve(dataEntity);
-            var A = Matrix<double>.Build.SparseOfArray(dataEntity.MatrixA);
-            var x = Vector<double>.Build.SparseOfArray(result);
-            double[] actual = A.Multiply(x).ToArray();
-
-            // Assert
-            Assert.All(actual, item => Assert.True(item <= accuracy));
+            return dataEntity;
         }
+
+        private BalanceDataModel GetDataModelV1WithConditions()
+        {
+            BalanceDataModel dataEntity = new BalanceDataModel()
+            {
+                MatrixA = new double[,] {
+                    { 1, -1, -1, 0, 0, 0, 0, -1 },
+                    { 0, 0, 1, -1, -1, 0, 0, 0 },
+                    { 0, 0, 0, 0, 1, -1, -1, 0 },
+                    { -1, 10, 0, 0, 0, 0, 0, 0 },
+                },
+                VectorY = new double[] { 0, 0, 0, 0 },
+                Tolerance = new double[] { 0.200, 0.121, 0.683, 0.040, 0.102, 0.081, 0.020, 0.667 },
+                VectorI = new double[] { 1, 1, 1, 1, 1, 1, 1, 1 },
+                VectorX0 = new double[] { 10.005, 3.033, 6.831, 1.985, 5.093, 4.057, 0.991, 6.667 }
+            };
+
+            return dataEntity;
+        }
+
+        #endregion
     }
 }
